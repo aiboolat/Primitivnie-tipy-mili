6707af7b6b83bbe7856473a3d4c34d7658cc4924
Aybulat <ayba.irgalin@gmail.com>
2023-02-26 18:57:18 +0600

    ispravilVse

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         int ticket = 20_000;
-        int BonusPrice = 20;
-        if (BonusPrice > 19) {
-            System.out.println(ticket / BonusPrice);
+        int bonusPrice = 20;
+        if (bonusPrice > 19) {
+            System.out.println(ticket / bonusPrice);
         } else {
             System.out.println(0);
         }
